8d94201b19d7c8819625b6e9340577e23531e49d
kvr3010 <wntjs789@gmail.com>
2020-07-05 20:39:34 +0900

    12주차 로그인 세션 sqllite

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template, session, url_for, abort, redirect
+app = Flask(__name__)
+import dbdb
+app.secret_key = '1234'
+
+
+@app.route('/')
+def test():
+    return render_template("test.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/result')
+def main():
+    if 'user' in session:
+        return render_template("main.html")
+    return redirect(url_for('login'))
+
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    num = request.form["num"]
+    name = request.form["name"]
+    if dbdb.select_num(num, name)!= None:
+          session['user'] = num
+          return '''
+                <script> alert("로그인 되었습니다");
+                location.href="/result"
+                </script>
+                '''
+    else:
+        return '''
+                <script> alert("학번 또는 이름을 확인하세요");
+                location.href="/result"
+                </script>
+                '''
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('login'))
+
+
+
+if __name__ == '__main__':
+     app.run(debug=True)
---
+++ dbdb.py
@@ -0,0 +1,61 @@
+import sqlite3
+
+def dbcon():
+    return sqlite3.connect('mydb.db')
+
+
+def create_table():
+    try:
+        db = dbcon()
+        c = db.cursor()
+        c.execute("CREATE TABLE student (num varchar(50), name varchar(50))")
+        db.commit()
+    except Exception as e:
+        print('db error:', e)
+    finally:
+        db.close()
+
+def insert_data(num, name):
+    try:
+        db = dbcon()
+        c = db.cursor()
+        setdata = (num, name)
+        c.execute("INSERT INTO student VALUES (?, ?)", setdata)
+        db.commit()
+    except Exception as e:
+        print('db error:', e)
+    finally:
+        db.close()
+
+def select_all():
+    ret = list()
+    try:
+        db = dbcon()
+        c = db.cursor()
+        c.execute('SELECT * FROM student')
+        ret = c.fetchall()
+    except Exception as e:
+        print('db error:', e)
+    finally:
+        db.close()
+        return ret
+
+
+def select_num(num,name):
+    ret = ()
+    try:
+        db = dbcon()
+        c = db.cursor()
+        setdata = (num, name)
+        c.execute('SELECT * FROM student WHERE (num = ? and name = ?)', setdata)
+        ret = c.fetchone()
+    except Exception as e:
+        print('db error:', e)
+    finally:
+        db.close()
+        return ret
+
+# create_table()
+# insert_data('20191068','박주선')
+# print(select_all())
+print(select_num('20191068','박주선'))
